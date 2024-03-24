2580751b80c37d9cd0ee54c2d089c931d44e14a2
Gustavo Sales <gustavo@thonlabs.io>
2024-03-24 18:08:37 -0300

    removes non-used endpoints / adds env example

--- apps/auth-services/src/modules/environments/controllers/environment.controller.ts
+++ apps/auth-services/src/modules/environments/controllers/environment.controller.ts
@@ -29,46 +29,6 @@ export class EnvironmentController {
     private emailTemplateService: EmailTemplateService,
   ) {}
 
-  @Get('/owner/my-keys')
-  @ThonLabsOnly()
-  public async signUpOwner(@Headers() headers) {
-    if (
-      headers['thon-labs-staff-api-key'] !== process.env.TL_INTERNAL_API_KEY
-    ) {
-      throw new UnauthorizedException();
-    }
-
-    const environmentId = 'env-production-pv58lzlj6te';
-    const [publicKey, secretKey] = await Promise.all([
-      this.environmentService.getPublicKey(environmentId),
-      this.environmentService.getSecretKey(environmentId),
-    ]);
-
-    return { publicKey, secretKey };
-  }
-
-  @Post('/:id/create-templates')
-  @ThonLabsOnly()
-  async createDefaultTemplates(
-    @Param('id') environmentId: string,
-    @Headers() headers,
-  ) {
-    if (
-      headers['thon-labs-staff-api-key'] !== process.env.TL_INTERNAL_API_KEY
-    ) {
-      throw new UnauthorizedException();
-    }
-
-    const result =
-      await this.emailTemplateService.createDefaultTemplates(environmentId);
-
-    if ((result as DataReturn)?.error) {
-      throw new exceptionsMapper[(result as DataReturn).statusCode](
-        (result as DataReturn).error,
-      );
-    }
-  }
-
   @Get('/:id')
   @ThonLabsOnly()
   @HasEnvAccess()
