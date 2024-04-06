ec0ae706373093a3424c33574feacba66fe71f01
Gustavo Sales <gustavo@thonlabs.io>
2024-04-06 10:22:00 -0300

    chore: increases throttler limit

--- apps/auth-services/src/app.module.ts
+++ apps/auth-services/src/app.module.ts
@@ -25,7 +25,7 @@ import { PublicKeyOrThonLabsOnlyGuard } from '@/auth/modules/shared/decorators/p
       throttlers: [
         {
           ttl: seconds(120),
-          limit: 10,
+          limit: 30,
         },
       ],
     }),
