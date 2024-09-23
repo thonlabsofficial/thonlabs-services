36e9a5adc1f9fb75a7c227fc7de626bf07029399
Gustavo Sales <gustavo@thonlabs.io>
2024-09-23 11:00:41 -0300

    feat: validate the confirm password when creating a new from reset

--- apps/auth-services/src/modules/auth/validators/reset-password-validators.ts
+++ apps/auth-services/src/modules/auth/validators/reset-password-validators.ts
@@ -13,10 +13,14 @@ export const requestResetPasswordValidator = z.object({
     .nullable(),
 });
 
-export const newPasswordValidator = z.object({
-  password: z
-    .string()
-    .regex(passwordPatterns.middleStrength)
-    .optional()
-    .nullable(),
-});
+export const newPasswordValidator = z
+  .object({
+    password: z
+      .string({ required_error: 'This field is required' })
+      .regex(passwordPatterns.middleStrength, { message: 'Password is weak' }),
+    confirm: z.string({ required_error: 'This field is required' }),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: 'Passwords must be equals',
+    path: ['confirm'],
+  });
