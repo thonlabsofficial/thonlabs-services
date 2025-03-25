23fe66182017dc54603e7be2d4962bcaf6dd4ddc
vitorjexs <vitortl@gmail.com>
2025-03-25 18:28:52 -0300

    fix regexp in const passwordPatterns

--- libs/utils/src/validators/password-patterns.ts
+++ libs/utils/src/validators/password-patterns.ts
@@ -1,5 +1,5 @@
 export const passwordPatterns = {
   middleStrength: new RegExp(
-    '^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8}$',
+    '^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$',
   ),
 };
