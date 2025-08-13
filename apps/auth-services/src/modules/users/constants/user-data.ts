987f4c0ae2c180711de458f5fb2cef95c92d8a51
Gustavo Sales <gus@thonlabs.io>
2025-08-13 08:44:19 -0300

    creates "user_data" table

---
+++ apps/auth-services/src/modules/users/constants/user-data.ts
@@ -0,0 +1,10 @@
+export enum UserSubscriptionType {
+  Basic = 'basic',
+  Pro = 'pro',
+  ProLifetime = 'proLifetime',
+  ProWaitlist = 'proWaitlist',
+}
+
+export enum UserDataKeys {
+  SubscriptionType = 'subscriptionType',
+}
